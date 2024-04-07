f057c27d39722efbcd47f681d808232b281d7ad7
l3lackk9 <116856005+l3lackk9@users.noreply.github.com>
2024-04-06 21:40:44 -0700

    Update config.mjs

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -3,7 +3,7 @@ import {
     SpellbookSlots,
     SpellbookMode,
     setSourceInfoByName,
-} from './pf1-utils.mjs';
+} from 'scripts/pf1-utils.mjs';
 
 function updateSpellBook(bookId, rollData, cache) {
     const actorData = this.system;
